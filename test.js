b3d25bec5c42751891d6c2da2dc66ec9afdd96f3
marcus.karrman <marcus.karrman@stendahls.se>
2022-08-23 14:36:47 +0200

    test

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
-dfdsfs
+const test = document.querySelectorAll('[data-type="virtual-form-table-row"');
+
+test.forEach((row, index) => {
+    console.log("1111", row);
+
+    let inputs = row.querySelectorAll(”input, textarea, select");
+
+    
+    inputs.forEach((input) => {
+        console.log('5555', input);
+        
+        input.addEventListener(’focus', (event) => {
+            console.log('Change on input in', index, input);
+        });
+        
+    })
+    
+
+    console.log('2222', inputs)
+});
