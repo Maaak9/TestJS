87499724ec2d7623f6ca8cc93850e4270072f1d7
marcus.karrman <marcus.karrman@stendahls.se>
2022-08-23 14:48:50 +0200

    WIP

--- test3.js
+++ test3.js
@@ -1,18 +1,17 @@
+console.log("aaabbbbccc");
 
-console.log('blablablalbalblalb');
-
-const test = document.querySelectorAll('[data-type="virtual-form-table-row"');
+let test = document.querySelectorAll('[data-type="virtual-form-table-row"');
 
 test.forEach((row, index) => {
     console.log("1111", row);
 
-    let inputs = row.querySelectorAll(”input, textarea, select");
+    let inputs = row.querySelectorAll("input");
 
-    
+     
     inputs.forEach((input) => {
         console.log('5555', input);
         
-        input.addEventListener(’focus', (event) => {
+        input.addEventListener("focus", (event) => {
             console.log('Change on input in', index, input);
         });
         
@@ -20,4 +19,7 @@ test.forEach((row, index) => {
     
 
     console.log('2222', inputs)
+
+   
+
 });
