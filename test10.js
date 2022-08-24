dd0985559343f83416eb2c597f51cd3e3984ea08
marcus.karrman <marcus.karrman@stendahls.se>
2022-08-24 09:51:46 +0200

    WIP

---
+++ test10.js
@@ -0,0 +1,38 @@
+console.log("aaabbbbccc");
+
+window.setTimeout(() => {
+  console.log("DOMContentLoaded triggered")
+  var rows = document.querySelectorAll('[data-type="virtual-form-table-row"');
+  console.log('rows', rows)
+  var rowsWithInput = [];
+  rows.forEach((row) => {
+    rowsWithInput.push(row.querySelectorAll("input, textarea, select"));
+  });
+  console.log('rowsWithInput', rowsWithInput)
+  // Filter out rows that don't have input, textarea, select
+  rowsWithInput = rowsWithInput.filter((inputs) => inputs.length > 0);
+
+  console.log('rowsWithInput after filter', rowsWithInput)
+
+  rowsWithInput.forEach((inputs, index) => {
+    console.log("1111", inputs);
+      
+    inputs.forEach((input) => {
+      console.log('5555', input);
+      
+      input.addEventListener("focus", (event) => {
+        console.log('Change on input in', index, input);
+
+        if (window.dataLayer) {
+            window.dataLayer.push({
+              event: "formEnterProgress",
+              formInfo: {
+                name: "testForm",
+                step: index,
+              },
+            });
+        }
+      });
+    })
+  });
+}, 500);
